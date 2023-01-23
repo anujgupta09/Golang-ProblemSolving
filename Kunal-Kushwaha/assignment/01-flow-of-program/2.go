f150a8f296770cb3f11e7c8d55fea794fa020ab0
anujgupta09 <1anujgupta123@gmail.com>
2023-01-23 11:50:08 +0530

    Done 2nd question

---
+++ Kunal-Kushwaha/assignment/01-flow-of-program/2.go
@@ -0,0 +1,14 @@
+//  2. Take two numbers and print the sum of both.
+
+package main
+
+import "fmt"
+
+func main() {
+	var num1 int
+	var num2 int
+	fmt.Scan(&num1)
+	fmt.Scan(&num2)
+
+	fmt.Println(num1 + num2)
+}
