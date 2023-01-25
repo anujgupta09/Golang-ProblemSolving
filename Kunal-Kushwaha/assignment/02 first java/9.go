15fa09e9d7c44f908db7e68581fe5bb7cc4b4528
anujgupta09 <1anujgupta123@gmail.com>
2023-01-25 16:16:54 +0530

    Actual que was to give all armstrong num between given 2 no.

--- Kunal-Kushwaha/assignment/02 first java/9.go
+++ Kunal-Kushwaha/assignment/02 first java/9.go
@@ -16,37 +16,20 @@ func main() {
 	fmt.Print("Enter num2 :")
 	fmt.Scan(&num2)
 
-	numbk1 := num1
-	numbk2 := num2
+	for i := num1; i <= num2; i++ {
+		var sumOfpower int
+		var decidePow = len(strconv.Itoa(i))
+		var j = i
+		for j > 0 {
+			lastDigit := j % 10
+			sumOfpower += int(math.Pow(float64(lastDigit), float64(decidePow)))
+			j /= 10
+		}
 
-	var sumOfpower1 int
-	var sumOfpower2 int
+		if sumOfpower == i {
+			fmt.Println("Sum of Cube is : ", sumOfpower, "|| Num is : ", i, " || decidePow is : ", decidePow, " || ", i, " is a Armstrong number")
+		}
 
-	var decidePow1 = len(strconv.Itoa(num1))
-	var decidePow2 = len(strconv.Itoa(num2))
-
-	for num1 > 0 {
-		lastDigit := num1 % 10
-		sumOfpower1 += int(math.Pow(float64(lastDigit), float64(decidePow1)))
-		num1 /= 10
-	}
-	for num2 > 0 {
-		lastDigit := num2 % 10
-		sumOfpower2 += int(math.Pow(float64(lastDigit), float64(decidePow2)))
-		num2 /= 10
 	}
 
-	fmt.Println("Sum of Cube is:", sumOfpower1, ", Num is:", numbk1, ", decidePow is:", decidePow1)
-	fmt.Println("Sum of Cube is:", sumOfpower2, ", Num is:", numbk2, ", decidePow is:", decidePow2)
-
-	if sumOfpower1 == numbk1 {
-		fmt.Println("num 1 is ArmStrong Number")
-	} else {
-		fmt.Println("num1 is Not Armstrong Number")
-	}
-	if sumOfpower2 == numbk2 {
-		fmt.Println("num 2 is ArmStrong Number")
-	} else {
-		fmt.Println("num 2 is Not Armstrong Number")
-	}
 }
