a459b040c6eba9c7ee41619469a418b60ba32671
anujgupta09 <1anujgupta123@gmail.com>
2023-01-25 14:10:48 +0530

    arstrong number withh dynaic power

---
+++ Kunal-Kushwaha/assignment/02 first java/9.go
@@ -0,0 +1,31 @@
+// 9. To find Armstrong Number between two given number.
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func main() {
+
+	var num int
+	fmt.Scan(&num)
+	numbk := num
+	var sumOfpower int
+	var decidePow = len(strconv.Itoa(num))
+
+	for num > 0 {
+		lastDigit := num % 10
+		sumOfpower += int(math.Pow(float64(lastDigit), float64(decidePow)))
+		num /= 10
+	}
+
+	fmt.Println("Sum of Cube is:", sumOfpower, ", Num is:", numbk, ", decidePow is:", decidePow)
+	if sumOfpower == numbk {
+		fmt.Println("ArmStrong Number")
+	} else {
+		fmt.Println("Not Armstrong Number")
+	}
+}
