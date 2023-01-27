79d1983126236966f8b8b670de825e47b8165026
anujgupta09 <1anujgupta123@gmail.com>
2023-01-27 17:29:49 +0530

    8 to 13

---
+++ Kunal-Kushwaha/assignment/03-conditionals-loops/8to13.go
@@ -0,0 +1,19 @@
+// 8. Perimeter Of Circle
+// 9. Perimeter Of Equilateral Triangle
+// 10. Perimeter Of Parallelogram
+// 11. Perimeter Of Rectangle
+// 12. Perimeter Of Square
+// 13. Perimeter Of Rhombus
+
+package main
+
+import "fmt"
+
+func main() {
+	var r float32
+	fmt.Scan(&r)
+
+	C := 2 * 3.14 * r
+	fmt.Println(C)
+
+}
