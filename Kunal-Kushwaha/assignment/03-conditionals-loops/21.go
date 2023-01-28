ac21d06ccf961f191c8d5fb5b5b48e046c41bfcc
anujgupta09 <1anujgupta123@gmail.com>
2023-01-28 12:09:38 +0530

    Roll backed to float64 version

--- Kunal-Kushwaha/assignment/03-conditionals-loops/21.go
+++ Kunal-Kushwaha/assignment/03-conditionals-loops/21.go
@@ -4,30 +4,22 @@ package main
 
 import (
 	"fmt"
-	"math/big"
 )
 
 func main() {
-	// var c float64 = 1
-	// var p float64 = 0
-	// var n float64
-	c := big.NewInt(1)
-	p := big.NewInt(0)
-	c = big.Int(12)
-	var n int
+	var c float64 = 1
+	var p float64 = 0
+	var n float64
 	fmt.Println("Enter nth number")
 	fmt.Scan(&n)
-	var fiboSeries []big.Int
-
-	var i int
+	var fiboSeries []float64
+	var i float64
 	for i = 1; i <= n; i++ {
-		fmt.Println("P : ", p)
-		fiboSeries = append(fiboSeries, *p)
-		cbk := c
-		c := new(big.Int)
-		fmt.Println("c : ", c)
-		c.Add(c, p)
+		fiboSeries = append(fiboSeries, p)
+		cbk := c  //as these statement is after 'append and print' last loop is not of any use
+		c = c + p // but as we are giving 1st value from p directly loop of 10 is enough for generating all 10 values
 		p = cbk
 	}
-	// fmt.Println(fiboSeries, " >>>  Upto ", n, " numbers ")
+	fmt.Println(fiboSeries, " >>>  Upto ", n, " numbers ")
+
 }
