0a3e4e07667d8a207a74c62e730a29d2eb8ee66d
anujgupta09 <1anujgupta123@gmail.com>
2023-02-03 23:37:22 +0530

    v1 basic

---
+++ Kunal-Kushwaha/assignment/03-conditionals-loops/Intermediate java Program/16.go
@@ -0,0 +1,20 @@
+// 16. Reverse A String In Java
+
+package main
+
+import "fmt"
+
+func main() {
+	var string1 string
+	fmt.Scan(&string1)
+	var array []string = []string{}
+	var revString string
+	for i := 0; i < len(string1); i++ {
+		strChar := string(string1[i])
+		array = append(array, strChar)
+	}
+	for i := len(array) - 1; i >= 0; i-- {
+		revString += array[i]
+	}
+	fmt.Println(revString)
+}
