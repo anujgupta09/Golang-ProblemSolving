750366ab0d6b817f77419f5488cc99886d3d34b1
anujgupta09 <1anujgupta123@gmail.com>
2023-07-18 14:12:22 +0530

    Updated with comments

--- Kunal-Kushwaha/assignment/05 arrays/easy/14.go
+++ Kunal-Kushwaha/assignment/05 arrays/easy/14.go
@@ -8,12 +8,12 @@ package main
 import "fmt"
 
 func main() {
-	m := 300
-	n := 10
+	m := 6
+	n := 5
 	indices := [][]int{
 		{0, 1},
 		{1, 1},
-		{200, 3},
+		{2, 3},
 	}
 	fmt.Println("oddCells function : ", oddCells(m, n, indices))
 }
@@ -22,29 +22,39 @@ func main() {
 
 func oddCells(m int, n int, indices [][]int) int {
 
-	var n1 = len(indices)
-	count := 0
+	var lenOfIndice = len(indices) // taking len of indices array
+	count := 0                     // intialize to 0
 
-	array := make([][]int, m)
-	for i := 0; i < m; i++ {
-		array[i] = make([]int, n)
+	// creating array >>>>>>>>>>
+
+	array := make([][]int, m) // making empty array , size is rows i.e 'm' here
+	for i := 0; i < m; i++ {  // loop to iterate in array and create cloumns using 'n'
+		array[i] = make([]int, n) // size is n here
 		for j := 0; j < n; j++ {
 			array[i][j] = 0
 		}
 	}
 	fmt.Println("array    : ", array)
 
-	for i := 0; i < n1; i++ {
+	// creating array <<<<<<<<<<
 
-		i1, i2 := indices[i][0], indices[i][1]
+	// iterrating over indices array and updating the main array as per conditions >>>
 
+	for i := 0; i < lenOfIndice; i++ {
+
+		i1, i2 := indices[i][0], indices[i][1] // fixed zero in indices[][0] means it will retrieve 0th positition in all elements of indices array which is again a array with 2 element [pos1,pos2] , similarly indices[][1] for 2nd position
+
+		fmt.Println("---------------------------------------")
 		fmt.Println("i1 : ", i1, "i2 : ", i2)
+		fmt.Println("---------------------------------------")
 
-		for j := 0; j < len(array); j++ {
+		// we took the first indices and now incrementing based on that indices
+
+		for j := 0; j < len(array); j++ { // iterate to row of array
 			fmt.Println("array j : ", array[j], "j : ", j)
-			for k := 0; k < len(array[j]); k++ {
+			for k := 0; k < len(array[j]); k++ { // iterate to column or row of array
 				// main logic
-				if j == i1 {
+				if j == i1 { // means row is = to i1
 					array[j][k] += 1
 					fmt.Println("array main logic : ", array)
 				}
@@ -55,16 +65,23 @@ func oddCells(m int, n int, indices [][]int) int {
 				// main logic
 			}
 		}
+		fmt.Println("============================================")
 	}
+
+	// iterating over indices array and updating the main array as per conditions <<<
+
+	// updating count as we got the final updated array >>>
+
 	for i := 0; i < len(array); i++ {
 		for j := 0; j < len(array[i]); j++ {
 			fmt.Println("array i j : ", array[i][j])
-			if array[i][j]%2 == 1 {
+			if array[i][j]%2 == 1 { // to check odd no. or not
 				count += 1
 			}
 		}
 	}
 	fmt.Println(array)
+	// updating count as we got the final updated array >>>
 
 	return count
 }
