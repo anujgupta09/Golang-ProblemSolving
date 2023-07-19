6b3290be02be65390498a7eb03fc29293e9bbb08
anujgupta09 <1anujgupta123@gmail.com>
2023-07-19 09:15:57 +0530

    Solution from discussion 15.go array easy

--- Kunal-Kushwaha/assignment/05 arrays/easy/15.go
+++ Kunal-Kushwaha/assignment/05 arrays/easy/15.go
@@ -8,28 +8,49 @@ import "fmt"
 
 func main() {
 	mat := [][]int{
-		{1, 2, 3, 1},
-		{3, 2, 1, 1},
-		{2, 3, 1, 1},
-		{1, 2, 3, 1},
+		{5},
 	}
 	fmt.Println(diagonalSum(mat))
 }
+
+// solution from discussion better !!
 func diagonalSum(mat [][]int) int {
+	leng := len(mat)
+	mid := leng / 2
+	sum := 0
+	fmt.Println("leng , mid : ", leng, mid)
 
-	sumOfAllDiagonalEle := 0
-	var diagonalEle1 = 0
-	var diagonalEle2 = len(mat) - 1
-
-	for row := 0; row <= len(mat)-1; row++ { // iterating over row
-		for column := 0; column <= len(mat[row])-1; column++ { // iterating over column
-			fmt.Println("daiEle1,diaEle2 :", diagonalEle1, diagonalEle2)
-			if column == diagonalEle1 || column == diagonalEle2 { //we already know diagonal position for the current row comparing column position with the same
-				sumOfAllDiagonalEle += mat[row][column]
-			}
-		}
-		diagonalEle1++
-		diagonalEle2--
+	for i := 0; i <= leng-1; i++ {
+		sum += mat[i][i] + mat[i][leng-1-i] //main logic
 	}
-	return sumOfAllDiagonalEle
+
+	if leng%2 != 0 {
+		sum -= mat[mid][mid]
+	}
+	return sum
 }
+
+// my solution
+
+// func diagonalSum(mat [][]int) int {
+
+// 	sumOfAllDiagonalEle := 0
+// 	var diagonalEle1 = 0
+// 	var diagonalEle2 = len(mat) - 1
+
+// 	for row := 0; row <= len(mat)-1; row++ { // iterating over row
+
+// 		for column := 0; column <= len(mat[row])-1; column++ { // iterating over column
+
+// 			fmt.Println("daiEle1,diaEle2 :", diagonalEle1, diagonalEle2)
+
+// 			if column == diagonalEle1 || column == diagonalEle2 { //we already know diagonal position for the current row comparing column position with the same
+// 				sumOfAllDiagonalEle += mat[row][column]
+// 			}
+// 		}
+
+// 		diagonalEle1++
+// 		diagonalEle2--
+// 	}
+// 	return sumOfAllDiagonalEle
+// }
