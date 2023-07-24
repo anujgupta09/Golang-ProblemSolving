28247b27600a2c28f8a4c6535210d4eff3a2da48
anujgupta09 <1anujgupta123@gmail.com>
2023-07-24 20:24:12 +0530

    17.go

--- Kunal-Kushwaha/assignment/05 arrays/easy/17.go
+++ Kunal-Kushwaha/assignment/05 arrays/easy/17.go
@@ -8,29 +8,42 @@ import "fmt"
 func main() {
 	var matrix = [][]int{
 		{1, 2},
-		{4, 5},
-		{7, 8},
+		{4, 5, 6},
+		{7},
+		{8, 9},
+		{10, 11},
 	}
 	fmt.Println(transpose(matrix))
 }
 func transpose(matrix [][]int) [][]int {
 
 	// transposeMatrix := matrix // this just make it another reference of matrix array hence change to one changes another too
-	var transposeMatrix [][]int = make([][]int, len(matrix)) // intialize array with make
 
-	for rowIndex, row := range matrix {
+	maxLenghtOfColumnToDecideRowOfTransposeMatrix := 0
+	rowLengthMap := map[int]int{}
 
-		transposeMatrix[rowIndex] = make([]int, len(row)) // intialize array inside array
+	// loop to decide rows in transpose matrix
+	for rowIndex, _ := range matrix {
 
-		for colIndex, _ := range row {
-
-			fmt.Println("row no. :", rowIndex, " |  col no. :", colIndex)
-			fmt.Println("Element at this postition : ", matrix[rowIndex][colIndex])
-			transposeMatrix[rowIndex][colIndex] = matrix[colIndex][rowIndex]
-			fmt.Println("------------------------------------------------")
-
+		if len(matrix[rowIndex]) > maxLenghtOfColumnToDecideRowOfTransposeMatrix {
+			maxLenghtOfColumnToDecideRowOfTransposeMatrix = len(matrix[rowIndex])
 		}
 	}
-	fmt.Println("matrix : ", matrix)
-	return transposeMatrix
+	for i := 0; i < maxLenghtOfColumnToDecideRowOfTransposeMatrix; i++ {
+		for rowIndex, _ := range matrix {
+			count := 0
+			var i int
+			if matrix[rowIndex][i] <= 0 || matrix[rowIndex][i] >= 0 {
+				count += 1
+				continue
+			}
+			fmt.Println(rowLengthMap[i])
+			rowLengthMap[i] = count
+		}
+
+	}
+
+	fmt.Println("map : ", rowLengthMap)
+	// var transposeMatrix [][]int = make([][]int, maxLenghtOfColumnToDecideRowOfTransposeMatrix) // intialize array with make
+	return matrix
 }
